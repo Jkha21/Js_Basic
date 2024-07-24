2525d1b48246da9617d7cb438322ceb6a91e62f6
jkha21 <hkumar3516@gmail.com>
2024-07-24 15:06:33 +0530

    Adding Asynchronous Function

---
+++ async.js
@@ -0,0 +1,12 @@
+
+// Asynchronous Function 
+function printMe() {
+    console.log('print me');
+  }
+  
+  function test() {
+    console.log('test');
+  }
+  
+  setTimeout(printMe, 2000);
+  test();
