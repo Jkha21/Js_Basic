a8bfd5c7496fd2184f85cbb1183179da841253c4
jkha21 <hkumar3516@gmail.com>
2024-07-25 17:29:06 +0530

    Removing the File

---
+++ data_type.js
@@ -0,0 +1,21 @@
+// Primitive Data Types 
+let num = 2; // Integer 
+let num2 = 1.3; // Floating point number
+let num3 = Infinity; // Infinity
+let num4 = 'something here too'/2; // NaN
+
+
+let str = "Hello There"; // String 
+
+
+// Boolean Values 
+let isCoding = true; // yes
+let isOld = false;      // no
+let age = null; // Null Data Type
+
+let x;
+console.log(x);  // undefined
+
+let symbol1 = Symbol("Geeks") // New Primitive Data Type
+
+
