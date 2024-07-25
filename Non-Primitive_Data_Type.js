442c656b31d976282a67471a7a3ccc652a7f8d0c
jkha21 <hkumar3516@gmail.com>
2024-07-25 11:01:51 +0530

    Removing the files

--- Non-Primitive_Data_Type.js
+++
@@ -1,5 +0,0 @@
-let person = new Object();
-// Object Constructor 
-
-let person1 = {};  
-// Object Literal
--- hoisting.js
+++
@@ -1,8 +0,0 @@
-let peter = new Person('Peter', 25);  // Class Hoisting
-console.log(peter);
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-}
