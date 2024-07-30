2bbb59f2e7d958e9a4ef9167a74dc93f81a29512
jkha21 <hkumar3516@gmail.com>
2024-07-30 12:53:38 +0530

    Adding the Hoisting Feature

--- hoisting.js
+++ hoisting.js
@@ -1,8 +1,26 @@
-let peter = new Person('Peter', 25);  // Class Hoisting
-console.log(peter);
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+console.log(greet()); // Outputs: "Hello, world!"
+
+function greet() {
+    return "Hello, world!";
 }
+
+// Function Hoisting
+
+console.log(myVar); // Outputs: undefined
+var myVar = 5;
+console.log(myVar); // Outputs: 5
+
+var myVar; // Declaration is hoisted
+console.log(myVar); // Outputs: undefined
+myVar = 5; // Assignment
+console.log(myVar); // Outputs: 5
+
+// Variable Hoisting 
+
+
+console.log(a); // Output: undefined
+var a = 10;
+console.log(a); // Output: 10
+
+// "var" declarations 
+
